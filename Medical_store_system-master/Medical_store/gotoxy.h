5aba449671f5de50b880d85dcc526fdebca4bf44
roshan971998 <61044051+roshan971998@users.noreply.github.com>
2021-06-22 19:18:58 +0530

    Delete Medical_store_system-master directory

--- Medical_store_system-master/Medical_store/gotoxy.h
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-COORD coord= {0,0}; // this is global variable
-void gotoxy(int x,int y)
-{
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
